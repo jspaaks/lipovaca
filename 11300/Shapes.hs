ad7b7705fbb6878d6856569df97623aa299ac190
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-11-08 17:40:46 -0500

    made Shape an instance of type class Show and implemented custom show method

--- 11300/Shapes.hs
+++ 11300/Shapes.hs
@@ -10,7 +10,24 @@ module Shapes ( Point2
     data Point2 = Point2 Float Float deriving (Show, Eq)
     data Shape2 = Circle Point2 Float
                 | Rect Point2 Point2
-                deriving (Show, Eq)
+                deriving (Eq)
+
+    instance Show Shape2 where
+        show shape = case shape of
+            Circle (Point2 x y) r
+                | x == 0 && y == 0 -> "mkCircle " <> show r
+                | x /= 0 && y == 0 -> "nudgx " <> show x <> " $ mkCircle " <> show r
+                | x == 0 && y /= 0 -> "nudgy " <> show y <> " $ mkCircle " <> show r
+                | otherwise        -> "nudgx " <> show x <> " $ nudgy " <> show y <> " $ mkCircle " <> show r
+            Rect (Point2 left top) (Point2 right bottom)
+                | left == 0 && bottom == 0 -> "mkRect " <> show w <> " " <> show h
+                | left /= 0 && bottom == 0 -> "nudgx " <> show left <> " $ mkRect " <> show w <> " " <> show h
+                | left == 0 && bottom /= 0 -> "nudgy " <> show bottom <> " $ mkRect " <> show w <> " " <> show h
+                | otherwise                -> "nudgx " <> show left <> " $ nudgy " <> show bottom <> " $ mkRect " <> show w <> " " <> show h
+                where
+                    w = right - left
+                    h = top - bottom
+
 
     mkPoint2 x y = Point2 x y
 
